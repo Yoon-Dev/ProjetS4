7a2b62700337bab3b045361bbec24f88ac48f657
Ubuntu <ubuntu@ip-172-31-80-17.ec2.internal>
2020-03-05 16:36:35 +0000

    Add new table and init backend

--- utils/con.php
+++
@@ -1,8 +0,0 @@
-<?php
-// connect to bdd
-
-$conn = new PDO('mysql:host=localhost;dbname=RessourceManager;charset=utf8', 'YooN', 'pass');
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échouéconn
-function chargerclass($classname, $dossier){
-    require $dossier.$classname.".php";
-}
